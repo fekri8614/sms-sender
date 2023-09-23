4bbc85eea9de854b43186a03febe7b45cbab59f6
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-09-23 08:05:01 -0700

    init commit

--- keys.py
+++ keys.py
@@ -1,13 +1,5 @@
-account_sid = 'AC99ec01b40b26fec37607605bbf7e0928'
-auth_token = '492f43328dc3f7a0dea15291ef46a148'
+account_sid = 'TWILIO_ACCOUNT_SID'
+auth_token = 'TWILIO_AUTH_TOKEN'
 
-twilio_number = '+12517327284'
-target_number = '+989145915860'
-
-###
-# +16072167969 - saeed hosseinzadeh :-)
-# +989935371504
-# +989397743029
-#
-# ###
-
+twilio_number = 'TWILIO_PHONE_NUMBER'
+target_number = 'WITH_NO_SPACE'
